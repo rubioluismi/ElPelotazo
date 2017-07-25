50dc28ae0339b338f32d784006efc5ac67d25c97
rubioluismi <rubioluismi@gmail.com>
2017-07-25 20:59:19 +0200

    Dia0

---
+++ VersionInicial/src/main/java/com/controllers/pelotazoController.java
@@ -0,0 +1,10 @@
+package com.controllers;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class pelotazoController {
+	
+	 protected final Log logger = LogFactory.getLog(getClass());
+
+}
